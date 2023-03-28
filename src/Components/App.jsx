1d6bd43f37dbdaf3cfa977a0dbd41ec0ea91f603
MauricioCastillo211106 <211106@ids.upchiapas.edu.mx>
2023-03-28 00:05:55 -0600

    add grafica humedad

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -4,6 +4,7 @@ import "../Assets/App.css"
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Sign_Up from "./Login.jsx"
 import Sign_In from "./Register.jsx"
+import Grafica from "./Grafica.jsx";
 
 function App() {
     return (
@@ -11,6 +12,7 @@ function App() {
             <Routes>
                 <Route path="/" element={<Sign_Up/>} />
                 <Route path="/Register" element={<Sign_In/>}/>
+                <Route path="/GraficHum" element={<Grafica/>}/>
             </Routes>
         </BrowserRouter>
     )
---
+++ src/Components/Grafica.jsx
@@ -0,0 +1,46 @@
+import React, { useState } from "react";
+import AnyChart from 'anychart-react';
+import '../Assets/Grafica.css'
+
+const variable = 60.00;
+
+
+export default function Grafica(){
+
+
+
+    const complexSettings = {
+        width: 600,
+        height: 500,
+        type: 'column',
+        data: [variable] ,
+        title: 'Porcentaje de Humedad',
+        yAxis: [1, {
+            orientation: 'right',
+            enabled: true
+        }],
+        legend: {
+            background: 'lightgreen 2',
+            padding: 0
+        },
+        lineMarker: {
+            value: 60
+        }
+    };
+    return (
+        <div className="container-fluid text-center d-flex justify-content-center">
+
+           <div className="card-group w-50  align-items-center">
+               <div className="card border-light mb-3 align-items-center " >
+                   <div className="card-header">Humedad</div>
+                   <div className="card-body">
+                       <AnyChart
+                           {...complexSettings}
+                       />
+                   </div>
+               </div>
+           </div>
+
+       </div>
+    )
+}
